b38ba517adc4770fbdefa7c144881076dba40601
Ashish Pimpre <50199708+synerf@users.noreply.github.com>
2022-01-05 15:39:49 +0530

    Initial commit - add splash screen

---
+++ app/src/main/java/com/synerf/pretask/SplashActivity.kt
@@ -0,0 +1,37 @@
+package com.synerf.pretask
+
+import android.annotation.SuppressLint
+import android.graphics.Typeface
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.synerf.pretask.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // make splash activity fullscreen
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        // setting custom font and using it for this one text (app name)
+        val typeFace: Typeface = Typeface.createFromAsset(assets, "carbon bl.ttf")
+        binding.tvAppName.typeface = typeFace
+    }
+}
